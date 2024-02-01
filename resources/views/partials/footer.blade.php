e86499e23011805322466ae31d8c1d1b2e1d968c
AlessandroSemeraro <alesemeraro1994@gmail.com>
2024-02-01 09:38:51 +0100

    added top-footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,71 @@
 <footer>
-    <section class="top-footer df-jc">
+    <section class="footer-container">
+        <div class="top-footer df-jc">
             <div class="container">
-                
+                <div class="container-col-footer">
+                    <div class="col-dc-footer">
+                        <h3>DC comics</h3>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                        <h3>shop</h3>
+                        <ul>
+                            <li>shop DC</li>
+                            <li>shop DC Collectibles</li>
+                        </ul>
+                    </div>
+                    <div class="col-dc-footer">
+                        <h3>DC</h3>
+                        <ul>
+                            <li>Terms of Use</li>
+                            <li>Privacy policy(new)</li>
+                            <li>AD choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscription</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </div>
+                    <div class="col-dc-footer">
+                        <h3>Sites</h3>
+                        <ul>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="container-copyright">
+                    <p>All Site Content TM and &#169; 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>.All rights reserved. <br><a href="#">Cookies settings</a></p>
+                </div>
             </div>
+        </div>
+        <div class="bottom-footer df-jc">
+            <div class="container">
+            <button>Sign-up now!</button>
+            <div class="footer-socials-bar">
+                <p class="text-socials">Follow us</p>
+                <ul>
+                    <li><img src="{{ Vite::asset('resources/assets/laravel-comics/images/footer-facebook.png') }}" alt="facebook logo"></li>
+                    <li><img src="{{ Vite::asset('resources/assets/laravel-comics/images/footer-twitter.png') }}" alt="twitter logo"></li>
+                    <li><img src="{{ Vite::asset('resources/assets/laravel-comics/images/footer-youtube.png') }}" alt="youtube logo"></li>
+                    <li><img src="{{ Vite::asset('resources/assets/laravel-comics/images/footer-pinterest.png') }}" alt="pinterest logo"></li>
+                    <li><img src="{{ Vite::asset('resources/assets/laravel-comics/images/footer-periscope.png') }}" alt="periscope logo"></li>
+                </ul>
+            </div>
+            </div>
+        </div>
     </section>
 </footer>
